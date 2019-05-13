ef88625cce6376aa0b16327b8a0a8307c47370bb
liviaab <liviaab7@gmail.com>
2019-05-13 00:21:05 -0300

    Add basename to test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/page" >
     <Routes />
   </BrowserRouter>
 )
