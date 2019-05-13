f2dc14b4ce2477e63f009d539ef262b8fd81a5c2
liviaab <liviaab7@gmail.com>
2019-05-12 23:43:04 -0300

    Updates

---
+++ precache-manifest.b74fa42e1141defbc25635dcaf2b4dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c00d0f697529a3218d7e9cac31d6d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7a05082801fa3bd58a",
+    "url": "/static/css/main.618f12ee.chunk.css"
+  },
+  {
+    "revision": "2ef466320c5457b7b37e",
+    "url": "/static/js/2.31378aaf.chunk.js"
+  },
+  {
+    "revision": "6c7a05082801fa3bd58a",
+    "url": "/static/js/main.12820abb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ff026b9fa2e604bb72c8f57a74bdcea",
+    "url": "/static/media/me.2ff026b9.jpg"
+  },
+  {
+    "revision": "020f7e0a34aef4d42de2fb00b6fa19f7",
+    "url": "/static/media/pexels-photo-949587.020f7e0a.jpeg"
+  }
+]);
