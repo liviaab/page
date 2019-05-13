1fbf0f5696e3016d50e1dc2819e663d7b1fb87f7
liviaab <liviaab7@gmail.com>
2019-05-12 23:46:28 -0300

    Updates

--- precache-manifest.38fc03c27680437a22268c306d9ae0d4.js
+++ precache-manifest.38fc03c27680437a22268c306d9ae0d4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83c00d0f697529a3218d7e9cac31d6d3",
+    "revision": "b3c4b51c4b5b76390ef680f4f4560086",
     "url": "/index.html"
   },
   {
-    "revision": "6c7a05082801fa3bd58a",
+    "revision": "942e00fe521c13e2c6c4",
     "url": "/static/css/main.618f12ee.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.31378aaf.chunk.js"
   },
   {
-    "revision": "6c7a05082801fa3bd58a",
-    "url": "/static/js/main.12820abb.chunk.js"
+    "revision": "942e00fe521c13e2c6c4",
+    "url": "/static/js/main.96f8975c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
