e75e0d577ca9b4cc4d2d66d776d025a2a4760ab8
liviaab <liviaab7@gmail.com>
2019-08-18 22:33:40 -0300

    Add talks route and page. Fixing CSS of the component Menu - again

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { Landing, Projects } from './components/pages'
+import { Landing, Projects, Talks } from './components/pages'
 import MainTemplate from './components/templates'
 
 const withMainTemplate = (component, props) => (
@@ -14,6 +14,7 @@ const Routes = () => (
   <Switch>
       <Route exact path="/" component={props => <Landing {...props} />} />
       <Route exact path="/projects" component={props => withMainTemplate(<Projects />)} />
+      <Route exact path="/talks" component={props => withMainTemplate(<Talks />)} />
   </Switch>
 )
 
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,10 +2,12 @@ import Extras from './extras/Extras'
 import Home from './home/Home'
 import Landing from './landing/Landing'
 import Projects from './projects/Projects'
+import Talks from './talks/Talks'
 
 export {
   Extras,
   Home,
   Landing,
-  Projects
+  Projects,
+  Talks
 }
